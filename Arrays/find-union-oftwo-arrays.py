90108d15977714ac77e5dea2cd055fb4d2128781
shradhaagarwal01 <62115289+shradhaagarwal01@users.noreply.github.com>
2021-01-15 17:13:12 +0530

    Delete find-union-oftwo-arrays.py

--- Arrays/find-union-oftwo-arrays.py
+++
@@ -1,8 +0,0 @@
-def union(arr1,arr2):
-    count = len(set(arr1+arr2))
-    return count
-
-n,m = [int(x) for x in input().split()]
-arr1 = [int(x) for x in input().split()]
-arr2 = [int(x) for x in input().split()]
-print(union(arr1,arr2))
