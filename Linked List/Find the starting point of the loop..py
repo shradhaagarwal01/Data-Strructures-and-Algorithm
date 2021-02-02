719009a1c138c06411b5cea4d95faf4c4b08ec48
shradhaagarwal01 <62115289+shradhaagarwal01@users.noreply.github.com>
2021-02-02 20:36:27 +0530

    Update Find the starting point of the loop..py

--- Linked List/Find the starting point of the loop..py
+++ Linked List/Find the starting point of the loop..py
@@ -1,39 +1,35 @@
 # Python3 program to return first node of loop.
 class Node:
-	def __init__(self, key):
-		self.key = key
-		self.next = None
+    def __init__(self, key):
+        self.key = key
+        self.next = None
 
 def newNode(key):
-	temp = Node(key)
-	return temp
+    temp = Node(key)
+    return temp
 
 def printList(head):
-	while (head != None):
-		print(head.key, end = ' ')
-		head = head.next
-	print()
-	
+    while (head != None):
+        print(head.key, end = ' ')
+        head = head.next
+    print()
 
 def detectAndRemoveLoop(head):
-	if head is None or head.next is None:
-		return None
-	fast = head
-	slow = head
-	while fast and fast.next:
-		slow = slow.next
+    if head is None or head.next is None:
+        return None
+    fast = head
+    slow = head
+    while fast and fast.next:
+        slow = slow.next
         fast = fast.next.next
         if slow==fast:
-			break
-	if slow!=fast:
-    	return None
-	if fast==head:
-		while(fast.next!=head):
-			fast = fast.next
-        fast.next = None
+            break
+    if slow!=fast:
+        return None
+    if fast==head:
         return slow
-	else:
-		slow = head
+    else:
+        slow = head
         while(slow.next!=fast.next):
             slow = slow.next
             fast = fast.next
@@ -41,21 +37,18 @@ def detectAndRemoveLoop(head):
         return slow
 
 if __name__=='__main__':
-	
-	head = newNode(50)
-	head.next = newNode(20)
-	head.next.next = newNode(15)
-	head.next.next.next = newNode(4)
-	head.next.next.next.next = newNode(10)
+    head = newNode(50)
+    head.next = newNode(20)
+    head.next.next = newNode(15)
+    head.next.next.next = newNode(4)
+    head.next.next.next.next = newNode(10)
 
-	# Create a loop for testing 
-	head.next.next.next.next.next = head.next.next
+    # Create a loop for testing 
+    head.next.next.next.next.next = head.next.next
 
-	res = detectAndRemoveLoop(head)
-	
-	if (res == None):
-		print("Loop does not exist")
-	else:
-		print("Loop starting node is " +
-			str(res.key))
+    res = detectAndRemoveLoop(head)
+    if (res == None):
+        print("Loop does not exist")
+    else:
+        print("Loop starting node is " + str(res.key))
 
