50ec800ad8f60c8d23c3ce696fe8c5ec238ed00e
shradhaagarwal01 <62115289+shradhaagarwal01@users.noreply.github.com>
2021-02-02 16:29:03 +0530

    Create Check If Circular Linked List.py

---
+++ Linked List/Check If Circular Linked List.py
@@ -0,0 +1,9 @@
+def isCircular(head):
+    fast = head
+    slow = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow==fast:
+            return True
+    return False
